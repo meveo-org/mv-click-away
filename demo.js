0aae7c9e5dd63bce81baf301673d9fdccb4d57b3
nam.nhat <nam.nhat@manaty.net>
2020-02-21 22:01:59 +0700

    add theme

--- demo.js
+++ demo.js
@@ -1,10 +1,12 @@
 import { LitElement, html, css } from "lit-element";
 import "./mv-click-away.js";
+import "mv-font-awesome";
 
 export class MvClickAwayDemo extends LitElement {
   static get properties() {
     return {
-      showMessage: { type: Boolean, attribute: true }
+      showMessage: { type: Boolean, attribute: true },
+      open: { type: Boolean, attribute: true }
     };
   }
 
@@ -17,24 +19,49 @@ export class MvClickAwayDemo extends LitElement {
 
       .main {
         border: 1px solid black;
-        background: red;
-        color: white;
         width: 180px;        
         padding: 50px;
         margin: 20px auto;
-      }  
+      }
+      
+      mv-fa[icon="lightbulb"] {
+        font-size: 50px;
+        cursor: pointer;
+        margin: 20px;
+      }
+      
+      .theme {
+        display: flex;
+        justify-content: flex-start;
+      }
+      
+      .light {
+        background: red;
+        color: black;
+      }
+      
+      .dark {
+        background: #373E48;
+        color: #FFFFFF;
+      } 
     `;
   }
 
   constructor() {
     super();
     this.showMessage = false;
+    this.open = true;
   }
 
   render() {
+    const iconColor = `color: ${this.open ? "yellow" : ""}`;
+    const boxClass = this.open ? "light" : "dark";
     return html`
+    <div class="theme">
+      <mv-fa icon="lightbulb" style="${iconColor}" @click=${this.toggleLightBulb}></mv-fa>
+    </div>
     <mv-click-away @clicked-away=${this.clickedAway}>
-      <div class="main" @click=${this.clickedInside}>
+      <div class="main ${boxClass}" @click=${this.clickedInside}>
         ${!this.showMessage
           ? html`<h3>Click me to show hidden message!</h3>`
           : html``}
@@ -52,6 +79,15 @@ export class MvClickAwayDemo extends LitElement {
 
   clickedInside = () => {
     this.showMessage = true;
+  };
+
+  toggleLightBulb = () => {
+    this.open = !this.open;
+    if (this.open) {
+      this.theme = "light";
+    } else {
+      this.theme = "dark";
+    }
   };
 }
 
